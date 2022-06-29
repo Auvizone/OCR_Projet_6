3d4ce2bc667debc6f0071d9e73b59a8cba26436c
Auvizone <humphrey.preston11@gmail.com>
2022-06-29 09:36:51 +0200

    ajout récupération des données photographes

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,6 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    console.log(data)
+    const { name, portrait, city, country, price, tagline, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,9 +10,35 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const location = document.createElement('p');
+        location.textContent = city + ',' + ' ' + country;
+        location.classList.add('location')
+        const tag = document.createElement('p');
+        tag.textContent = tagline
+        tag.classList.add('tag')
+        const priceTag = document.createElement('p');
+        priceTag.textContent = price + '€/jour';
+        priceTag.classList.add('priceTag')
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location)
+        article.appendChild(tag)
+        article.appendChild(priceTag)
+        article.addEventListener('click', () => {
+            window.location.href = `http://127.0.0.1:5500/photographer.html?id=${id}`
+        })
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, price, tagline, getUserCardDOM }
 }
+
+    /** Transmettre l'id
+     * 
+     * @param id- L'id du photographre
+     * @return The photographer
+     */
+    function redirectPhotographer(id, test) {
+        console.log(test)
+        console.log(id)
+        
+    }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,26 @@
 //Mettre le code JavaScript lié à la page photographer.html
+    let params = (new URL(document.location)).searchParams;
+    let id = params.get('id');
+    console.log(id)
+
+    getPhotographers(id);
+    
+    async function getPhotographers(id) {
+        const response = await fetch('../../data/photographers.json');
+        const photographers = await response.json();
+        console.log(photographers)
+        findId(photographers, id);
+        return(photographers)
+    }
+
+    function findId(photographers, id) {
+        var data = photographers.photographers;
+        console.log(data)
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].id == id) {
+                console.log('photographe trouvé', data[i])
+                return(data[i])
+            }
+        }
+    }
+
